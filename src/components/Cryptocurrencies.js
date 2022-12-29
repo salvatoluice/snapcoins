4360bc43f0c66b58fb695f04e7fb653ffb6712c1
Salvato Luice <salvatoluice5@gmai.com>
2022-12-29 14:37:03 +0300

    Commit

--- src/components/Cryptocurrencies.js
+++ src/components/Cryptocurrencies.js
@@ -1,21 +1,36 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import millify from 'millify'
 import { Link } from 'react-router-dom'
 import { Card, Row, Col, Input } from 'antd'
 import { useGetCryptosQuery } from './services/cryptoApi'
 
-const Cryptocurrencies = () => {
+const Cryptocurrencies = ({ simplified }) => {
 
-  const { data: cryptosList,isFetching } = useGetCryptosQuery();
-  const [cryptos, setCryptos] = useState(cryptosList?.data?.coins); 
+  const count = simplified ? 10 : 100;
 
-  console.log(cryptos)
+  const { data: cryptosList,isFetching } = useGetCryptosQuery(count);
+  const [cryptos, setCryptos] = useState([]); 
+  const [searchTerm, setSearchTerm] = useState('');
+
+  useEffect(() => {
+    // setCryptos(cryptosList?.data?.coins);
+
+    const filteredData = cryptosList?.data?.coins.filter((item) => item.name.toLowerCase().includes(searchTerm));
+    setCryptos(filteredData);
+  }, [cryptosList, searchTerm])
+
+  // console.log(cryptos)
+
+  if (isFetching) return 'Loading...';
 
 
   return (
     <>
+      <div className='search-crypto'>
+        <Input placeholder='Search Coins...' onChange={(e) => setSearchTerm(e.target.value)} />
+      </div>
       <Row gutter={[32, 32]} className='crypto-card-container'>
-        {cryptos.map((currency) => (
+        {cryptos?.map((currency) => (
           <Col xs={24} sm={12} lg={6} className='crypto-card' key={currency.id}>
             <Link to={`/crypto/${currency.id}`}>
               <Card title={`${currency.rank}. ${currency.name}`} extra={<img className='crypto-image' src={currency.iconUrl} />} hoverable> 
